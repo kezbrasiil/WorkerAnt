77f049c6ccfc0ebe2279a0ab05c5249750732316
Wilker <wilkervinicius@gmail.com>
2017-09-03 20:35:41 -0300

    corrigido problema da direcional e sprite

---
+++ build.lua
@@ -0,0 +1,29 @@
+application = {
+	content = {
+		width = 360,
+		height = 500, 
+		--scale = "zoomEvent",
+		fps = 30,
+		
+		--[[
+        imageSuffix = {
+		    ["@2x"] = 2,
+		}
+		--]]
+	},
+
+    --[[
+    -- Push notifications
+
+    notification =
+    {
+        iphone =
+        {
+            types =
+            {
+                "badge", "sound", "alert", "newsstand"
+            }
+        }
+    }
+    --]]    
+}
---
+++ config.lua
@@ -0,0 +1,24 @@
+--
+-- For more information on config.lua see the Project Configuration Guide at:
+-- https://docs.coronalabs.com/guide/basics/configSettings
+--
+
+application =
+{
+	content =
+	{
+		width = 400,
+		height = 550, 
+		scale = "zoomEven",
+		fps = 60,
+		--audioPlayFrequency = 22050,
+		
+		--[[
+		imageSuffix =
+		{
+			    ["@2x"] = 2,
+			    ["@4x"] = 4,
+		},
+		--]]
+	},
+}
